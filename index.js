e055457002c8a867fe848283e987a69494036c8f
Adeyemi Arimoro <arimoro71@gmail.com>
2023-02-08 17:15:54 -0600

    Added code to index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  let mySum = a + b;
+  return mySum;
+}
+
+function subtract(a, b) {
+  let mySubtraction = a - b;
+  return mySubtraction;
+}
+
+function multiply(a, b) {
+  let myProduct = a * b;
+  return myProduct;
+}
+
+function divide(a, b) {
+  let myQuotient = a / b;
+  return myQuotient;
+}
+
+function increment(n) {
+  return (n += 1);
+}
+
+function decrement(n) {
+  return (n -= 1);
+}
+
+function makeInt(n) {
+  let myInt = parseInt(n, 10);
+  return myInt;
+}
+
+function preserveDecimal(n) {
+  let myDecimals = parseFloat(n);
+  return myDecimals;
+}
+
+
+
